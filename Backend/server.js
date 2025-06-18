35002f732860c87fef647a218fd5b6ecab3777aa
sahithdaddla <daddlasahith>
2025-06-18 12:13:05 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -164,5 +164,5 @@ app.delete('/api/claims', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.60.22.32:${port}`);
+    console.log(`Server running at http://16.171.181.29:${port}`);
 });
